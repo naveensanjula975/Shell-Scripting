aaef1c2fd132a7c47c0abb88279b5465663b8543
naveen sanjula <naveensanjula975@gmail.com>
2022-06-06 10:25:52 +0530

    small script

--- simple.sh
+++ simple.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 echo "Hello world! This is the first shell script"
+
+echo "Hi Mom"
+
+sleep 3
+
+echo "Uh uh"
+
+sleep 3
+
+echo "Oh wow"
+
+sleep 3
+
+echo "I can't believe that"
+
+sleep 3
+
+echo "Bye"
