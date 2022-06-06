ed125c83298010e3a0b82bc1a69ec5f61a439988
naveen sanjula <naveensanjula975@gmail.com>
2022-06-06 11:00:35 +0530

    rich

--- getRichQuick.sh
+++ getRichQuick.sh
@@ -11,3 +11,11 @@ read age
 echo "Hello $name, you are $age years old."
 
 echo "$PWD, $SHELL, $USER, $HOSTNAME" 
+
+sleep 2
+
+
+
+getrich=$((( $RANDOM % 15 ) + $age ))
+
+echo "$name, you will become a millionare when you are $getrich years old"
