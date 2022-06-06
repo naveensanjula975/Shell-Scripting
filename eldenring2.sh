36c6481b64fcd98e11c21a773852a030b6ce111e
naveen sanjula <naveensanjula975@gmail.com>
2022-06-06 12:26:44 +0530

    second code

--- eldenring2.sh
+++ eldenring2.sh
@@ -14,4 +14,19 @@ if [[ $beast == $tarnished ]]; then
         echo "Beast VANQUISHED!! Congrats fellow tarnished"
 else
         echo "You Died"
+        exit 1
+fi
+
+sleep 2
+
+echo "Boss battle. Get scared. It's Margit. Pick a number between 0-9. (0-9)"
+
+read tarnished
+
+beast=$(( $RANDOM % 10))
+
+if [[ $beast == $tarnished ]]; then
+        echo "Beast VANQUISHED!! Congrats fellow tarnished"
+else
+        echo "You Died"
 fi
